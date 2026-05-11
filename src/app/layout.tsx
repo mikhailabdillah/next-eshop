e0a01748d62a82767a2e4aa544c652c11f0a537f
mikhail <mikhailabdillahtr@gmail.com>
2026-05-11 16:28:32 +0700

    feat: add UI components, set favicons, fonts, etc

---
+++ src/app/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from 'next'
+import localfont from 'next/font/local'
+import './globals.css'
+import { cn } from '@/lib/utils'
+
+const beatrice = localfont({
+  variable: '--font-beatrice',
+  src: [
+    {
+      path: '../assets/fonts/BeatriceDeckTRIAL-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/BeatriceDeckTRIAL-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/BeatriceDeckTRIAL-Semibold.otf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/BeatriceDeckTRIAL-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/BeatriceDeckTRIAL-Extrabold.otf',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+})
+
+export const metadata: Metadata = {
+  title: 'Cloth Store',
+  description:
+    'A simple e-commerce application built with Shopify, Next.js, TypeScript, and Tailwind CSS.',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" className={cn('h-full', 'antialiased', beatrice.variable)}>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Cloth Store" />
+      </head>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="container mx-auto">
+        <h1 className="text-6xl">New Collection</h1>
+      </div>
+    </main>
+  )
+}
