3bd5e1f3aabff81a100cb7e9440cbabb8c440ae0
mikhail <mikhailabdillahtr@gmail.com>
2026-05-12 23:37:13 +0700

    refactor: standardize quotes and improve Navbar layout

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,25 +1,25 @@
-'use client'
+"use client"
 
-import Image from 'next/image'
-import Link from 'next/link'
-import logo from '@/assets/images/shop-logo.png'
-import { Button } from '../ui/button'
-import { HandbagIcon, HeartIcon, UserIcon } from 'lucide-react'
-import { useState } from 'react'
-import { Separator } from '../ui/separator'
-import { Input } from '../ui/input'
-import { Sidebar } from './Sidebar'
+import Image from "next/image"
+import Link from "next/link"
+import logo from "@/assets/images/shop-logo.png"
+import { Button } from "../ui/button"
+import { HandbagIcon, HeartIcon, UserIcon } from "lucide-react"
+import { useState } from "react"
+import { Separator } from "../ui/separator"
+import { Input } from "../ui/input"
+import { Sidebar } from "./Sidebar"
 
 const navItems = [
-  { name: 'Home', href: '/' },
-  { name: 'Collection', href: '/collection' },
-  { name: 'New', href: '/new' },
+  { name: "Home", href: "/" },
+  { name: "Collection", href: "/collection" },
+  { name: "New", href: "/new" },
 ]
 
 function Menu() {
   return (
     <>
-      <div className="flex flex-row items-center mb-6 gap-4">
+      <div className="flex flex-row items-center my-6 gap-4">
         <h2 className="text-2xl font-extrabold">Cloth Store</h2>
         <Image src={logo} alt="Cloth store" />
       </div>
@@ -103,84 +103,86 @@ export function Navbar() {
   }
 
   return (
-    <header>
+    <>
       <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar}>
         <Menu />
       </Sidebar>
-      <nav className="container mx-auto">
-        <div className="flex flex-row items-center w-full h-20 px-4">
-          <div className="basis-1/3">
-            <div className="flex flex-row items-center gap-4">
-              <Button
-                variant="ghost"
-                className="p-2 h-auto cursor-pointer"
-                onClick={toggleSidebar}
-              >
-                <svg
-                  width="28"
-                  height="18"
-                  viewBox="0 0 28 18"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-8"
+      <header className="py-6">
+        <nav className="container mx-auto">
+          <div className="flex flex-row items-center w-full h-24 px-4">
+            <div className="basis-1/3">
+              <div className="flex flex-row items-center gap-4">
+                <Button
+                  variant="ghost"
+                  className="p-2 h-auto cursor-pointer"
+                  onClick={toggleSidebar}
                 >
-                  <path
-                    d="M26.75 0.75L0.750001 0.75"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                  />
-                  <path
-                    d="M18.75 8.75L0.75 8.75"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                  />
-                  <path
-                    d="M13.75 16.75H0.75"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                  />
-                </svg>
-              </Button>
-              <ul className="hidden lg:flex gap-6 ms-4">
-                {navItems.map((item) => (
-                  <li key={item.name} className="">
-                    <Link
-                      href={item.href}
-                      className="text-lg font-medium text-primary hover:text-primary/60 transition-colors duration-200"
-                    >
-                      {item.name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+                  <svg
+                    width="28"
+                    height="18"
+                    viewBox="0 0 28 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-8"
+                  >
+                    <path
+                      d="M26.75 0.75L0.750001 0.75"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M18.75 8.75L0.75 8.75"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                    />
+                    <path
+                      d="M13.75 16.75H0.75"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                    />
+                  </svg>
+                </Button>
+                <ul className="hidden lg:flex gap-6 ms-4">
+                  {navItems.map((item) => (
+                    <li key={item.name} className="">
+                      <Link
+                        href={item.href}
+                        className="text-lg font-medium text-primary hover:text-primary/60 transition-colors duration-200"
+                      >
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="basis-1/3 flex justify-center">
+              <Link href="/">
+                <Image src={logo} alt="Cloth store" />
+              </Link>
+            </div>
+            <div className="basis-1/3 flex justify-end">
+              <div className="flex items-center gap-4">
+                <Button size={"icon-lg"} className="hidden lg:inline-flex">
+                  <HeartIcon className="size-6" />
+                  <span className="sr-only">Wishlist</span>
+                </Button>
+                <Button size={"icon-lg"} className="hidden lg:inline-flex">
+                  <HandbagIcon className="size-6" />
+                  <span className="sr-only">Cart</span>
+                </Button>
+                <Button size={"icon-lg"}>
+                  <UserIcon className="size-6" />
+                  <span className="sr-only">Profile</span>
+                </Button>
+              </div>
             </div>
           </div>
-          <div className="basis-1/3 flex justify-center">
-            <Link href="/">
-              <Image src={logo} alt="Cloth store" />
-            </Link>
-          </div>
-          <div className="basis-1/3 flex justify-end">
-            <div className="flex items-center gap-4">
-              <Button size={'icon-lg'} className="hidden lg:inline-flex">
-                <HeartIcon className="size-6" />
-                <span className="sr-only">Wishlist</span>
-              </Button>
-              <Button size={'icon-lg'} className="hidden lg:inline-flex">
-                <HandbagIcon className="size-6" />
-                <span className="sr-only">Cart</span>
-              </Button>
-              <Button size={'icon-lg'}>
-                <UserIcon className="size-6" />
-                <span className="sr-only">Profile</span>
-              </Button>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </>
   )
 }
