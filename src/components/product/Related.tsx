4acb1a4cbd1da0a312b17adf20468d8b36956226
mikhail <mikhailabdillahtr@gmail.com>
2026-05-14 11:50:22 +0700

    feat: replace related products list with carousel for improved UI/UX

--- src/components/product/Related.tsx
+++ src/components/product/Related.tsx
@@ -1,6 +1,7 @@
 import { getProductRecommendations } from "@/lib/shopify"
 import Link from "next/link"
 import Image from "next/image"
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel"
 
 export async function RelatedProducts({ id }: { id: string }) {
   const relatedProducts = await getProductRecommendations(id)
@@ -8,30 +9,37 @@ export async function RelatedProducts({ id }: { id: string }) {
   if (!relatedProducts.length) return null
 
   return (
-    <div className="py-8">
+    <div className="py-24">
       <h2 className="mb-4 text-4xl font-bold">Related Products</h2>
-      <ul className="flex w-full gap-4 overflow-x-auto pt-1">
-        {relatedProducts.map((product) => (
-          <li
-            key={product.handle}
-            className="aspect-square w-full flex-none min-[475px]:w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5"
-          >
-            <Link
-              className="relative h-full w-full"
-              href={`/product/${product.handle}`}
-              prefetch={true}
+      <Carousel
+        opts={{
+          dragFree: true,
+          skipSnaps: true,
+        }}
+      >
+        <CarouselContent>
+          {relatedProducts.map((product) => (
+            <CarouselItem
+              key={product.handle}
+              className="basis-3/4 md:basis-4/9 lg:basis-3/10 xl:basis-3/13"
             >
-              <Image
-                src={product.featuredImage?.url}
-                alt={product.title}
-                width={product.featuredImage?.width}
-                height={product.featuredImage?.height}
-                className="object-cover aspect-10/11"
-              />
-            </Link>
-          </li>
-        ))}
-      </ul>
+              <Link
+                className="relative h-full w-full"
+                href={`/product/${product.handle}`}
+                prefetch={true}
+              >
+                <Image
+                  src={product.featuredImage?.url}
+                  alt={product.title}
+                  width={product.featuredImage?.width}
+                  height={product.featuredImage?.height}
+                  className="object-cover aspect-10/11"
+                />
+              </Link>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </div>
   )
 }
